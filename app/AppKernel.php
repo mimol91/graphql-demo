b86dea535f6bf37b1bdc441187e07b1426c84f7f
Adam Szaraniec <adam.szaraniec@xsolve.pl>
2016-12-26 14:00:18 +0100

    grapgql

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Overblog\GraphQLBundle\OverblogGraphQLBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/AppBundle/GraphQL/Resolver/FooResolver.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\GraphQL\Resolver;
+
+class FooResolver
+{
+
+    public function resolveFoo()
+    {
+        return 'BAR';
+    }
+}
